83bab783b0dff3d0611af0715b6840c5aacce8bd
shyamkunarapudev <shyamkumar14318@gmail.com>
2025-02-17 05:03:53 +0000

    implimenting :using firebase user can signin/signup email/password/name

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,53 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { validation } from '../utils/validate'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../utils/firebase"
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const name = useRef(null)
   const email = useRef(null)
   const password = useRef(null)
 
   const handleButtonClick = () =>{
     const message = validation(email.current.value, password.current.value)
     setErrorMessage(message)
+    if(!message){
+      if(!isSignIn){
+        //signup
+        createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+          .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user)
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setErrorMessage(errorCode+" "+errorMessage )
+            // ..
+          });
+
+      }else{
+        signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+          .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user)
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setErrorMessage(errorCode+" "+errorMessage)
+          });
+      }
+    }
+
+    
   }
 
   const toggleSignInForm = () =>{
@@ -25,7 +62,7 @@ const Login = () => {
       </div>
       <form className="w-1/3 absolute text-white mt-20 p-14 rounded-lg mx-auto left-0 right-0 bg-black/80 " onSubmit={(e)=>e.preventDefault()}>
         <h1 className="mb-4 text-3xl font-bold">{isSignIn?"Sign In":"Sign Up"}</h1>
-        {!isSignIn&&<input className="w-full p-3 my-3 rounded-lg bg-slate-700 cursor-pointer" type='text' placeholder='Full name' />}
+        {!isSignIn&&<input ref={name} className="w-full p-3 my-3 rounded-lg bg-slate-700 cursor-pointer" type='text' placeholder='Full name' />}
         <input ref={email} className="w-full p-3 my-3 rounded-lg bg-slate-700 cursor-pointer" type='text' placeholder='Email address' />
         <input ref={password} className="w-full p-3 my-3 rounded-lg bg-slate-700 cursor-pointer" type='password' placeholder='Password'/>
         <p className="py-3 font-bold text-red-600">{errorMessage}</p>
