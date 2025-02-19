fa4029906e6aeb4d8a175eb3bb6005fa58633eb5
shyamkunarapudev <shyamkumar14318@gmail.com>
2025-02-19 07:03:29 +0000

    Decrease font size Genre

--- src/components/MovieListComponent.jsx
+++ src/components/MovieListComponent.jsx
@@ -5,7 +5,7 @@ const MovieListComponent = ({genre, data}) => {
   if(!data) return
   return (
     <div className='pb-2 px-4 text-white'>
-      <h1 className="text-3xl font-bold py-3 z-10 relative">{genre} &gt;</h1>
+      <h1 className="text-2xl font-bold py-3 z-10 relative">{genre} &gt;</h1>
       <div className="flex overflow-x-scroll scroll-smooth no-scrollbar">
         <div className="flex">
           {data.map((em)=><MovieCard movieInfo={em}/>)}
