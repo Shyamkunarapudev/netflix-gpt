4e0eeeb81c1a753243e0c89d5545e5f491e985b2
shyamkunarapudev <shyamkumar14318@gmail.com>
2025-02-19 07:08:56 +0000

    Increase the width of Play & More Info & some other

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -46,8 +46,8 @@ const MainContainer = () => {
       </div>
       <div className="w-screen h-screen absolute bg-gradient-to-r from-black top-0 left-0 pt-20 px-20 z-10  text-white">
         <h1 className="font-bold text-4xl z-10 my-6 w-1/3">{findMovie[0]?.Title}</h1>
-        <p className="font-semibold text-sm/5 w-1/4">{findMovie[0]?.Plot}</p>
-        <div className="w-1/4 flex mt-6 gap-3">
+        <p className="font-semibold text-sm/5 w-1/3">{findMovie[0]?.Plot}</p>
+        <div className="w-1/3 flex mt-6 gap-3">
           <button className="w-1/2 py-2 text-xl bg-white/80 text-black rounded-lg font-semibold" >Play</button>
           <button className="w-1/2 py-2 text-xl rounded-lg font-semibold bg-slate-400/50">More Info</button>
         </div>
