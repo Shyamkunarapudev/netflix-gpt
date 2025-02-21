87c81faad7705b0dae3f47b17f9c4ea80477c1a2
shyamkunarapudev <shyamkumar14318@gmail.com>
2025-02-21 15:00:35 +0000

    *Movie Info to Every MovieCard, here infoSlice acts as Placeholer for movie info && MoreInfo component acts Reasuable compontent

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { addMovieInfo } from '../utils/slices/infoSlice'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const MovieCard = ({movieInfo}) => {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const infoHandler =()=>{
+    dispatch(addMovieInfo(movieInfo))
+    navigate('/browse/info')
+  }
+
   return (
     <div className='w-36 mr-2 z-10'>
-      <img className='rounded-lg' src={movieInfo.Poster} alt="" />
+      <img className='rounded-lg' src={movieInfo.Poster} alt="Movie Poster" onClick={infoHandler} />
     </div>
   )
 }
